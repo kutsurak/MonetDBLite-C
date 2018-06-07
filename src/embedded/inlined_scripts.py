7d98d8d737ca6dbc3f84a02c6a97ad4eaba89700
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2018-06-07 12:42:24 +0200

    Small changes for PEP8 compliance
    The most important change is the "match == None" to "match is
    None". See the second bullet at
    https://www.python.org/dev/peps/pep-0008/#programming-recommendations
    
    The rest are mainly cosmetic changes to make the code easier to read
    for python programmers.

--- src/embedded/inlined_scripts.py
+++ src/embedded/inlined_scripts.py
@@ -6,6 +6,7 @@ import zlib
 # these includes are skipped
 blacklist = ('remote', 'srvpool', 'mal_mapi', 'json', 'json_util', 'mcurl', 'xml', 'batxml', 'recycle', 'txtsim', 'tokenizer', 'zorder', '70_vault', '80_lsst', '80_udf', '23_skyserver', '24_zorder', '40_json', '80_udf')
 
+
 def mal_include(filename):
     if os.path.isdir(filename):
         files = os.listdir(filename)
@@ -16,12 +17,13 @@ def mal_include(filename):
                 ret += mal_include(os.path.join(filename, f))
         return ret
     elif os.path.isfile(filename):
-        print filename
+        print(filename)
         content = open(filename).read() + "\n"
         content = re.sub("^#.*$", "", content, flags=re.MULTILINE)
         while True:
             match = re.search("include (\\w+);", content)
-            if (match == None): break
+            if match is None:
+                break
             modname = match.groups(0)[0]
             if not modname.startswith(blacklist):
                 incfile = mal_include(modname + ".mal" if os.path.isfile(modname + ".mal") else modname)
@@ -30,15 +32,17 @@ def mal_include(filename):
     else:
         return ""
 
+
 def to_hex(s, n=1024):
     result = "{"
     for chunk in [s[i:i+n] for i in range(0, len(s), n)]:
-        result += ",".join(str(ord(c)) for c in chunk) +  ",\n"
+        result += ",".join(str(ord(c)) for c in chunk) + ",\n"
     return "\n" + result + "0}"
+
 
 os.chdir(sys.argv[1])
 mi = mal_include("mal_init.mal")
-#print(mi)
+# print(mi)
 s = zlib.compress(mi, 9)
 outf = open(sys.argv[2], "w")
 outf.write("unsigned char mal_init_inline_arr[] = " + to_hex(s) + ";\n")
@@ -54,4 +58,3 @@ for f in files:
 s = zlib.compress(s, 9)
 outf.write("\nunsigned char createdb_inline_arr[] = " + to_hex(s) + ";\n")
 outf.write("unsigned char* createdb_inline = 0;\n")
-
